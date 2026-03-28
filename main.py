568f49becad3bfa2231b905d00ea53ff2b6a577e
Jayden <97096023@st.deltion.nl>
2026-03-28 19:27:13 +0100

    Cleaning the data

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import pandas as pd
+import numpy as np 
 
 
 df = pd.read_csv('data/diabetes.csv')
 
+df['Glucose'] = df['Glucose'].replace(0, np.nan)
+df['Glucose'] = df['Glucose'].fillna(df['Glucose'].mean())
+
+df['BloodPressure'] = df['BloodPressure'].replace(0, np.nan)
+df['BloodPressure'] = df['BloodPressure'].fillna(df['BloodPressure'].mean())
+
+df['SkinThickness'] = df['SkinThickness'].replace(0, np.nan)
+df['SkinThickness'] = df['SkinThickness'].fillna(df['SkinThickness'].mean())
+
+df['Insulin'] = df['Insulin'].replace(0, np.nan)
+df['Insulin'] = df['Insulin'].fillna(df['Insulin'].mean())
+
+df['BMI'] = df['BMI'].replace(0, np.nan)
+df['BMI'] = df['BMI'].fillna(df['BMI'].mean())
+
+print(df['Glucose'] == 0)
+print(df['BloodPressure'] == 0)
+print(df['SkinThickness'] == 0)
+print(df['Insulin'] == 0)
+print(df['BMI'] == 0)
